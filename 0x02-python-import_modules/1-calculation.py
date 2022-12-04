514343a2e80ceae207286072d9e5f15571d4fe36
elolengodfrey <elolengodfrey@gmail.com>
2022-12-04 03:59:10 -0800

    0

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,4 +6,12 @@ a = 10
 b = 5
 
 c = add(a, b)
+d = sub(a, b)
+e = mul(a, b)
+f = div(a, b)
+
 print("{} + {} = {}".format(a, b, c))
+print("{} - {} = {}".format(a, b, d))
+print("{} * {} = {}".format(a, b, e))
+print("{} / {} = {}".format(a, b, f))
+
