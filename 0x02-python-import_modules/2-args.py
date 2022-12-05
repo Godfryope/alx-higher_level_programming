a0e6b3126a6ca15828e3f9f1d9cabc1407bd1d61
elolengodfrey <elolengodfrey@gmail.com>
2022-12-05 00:01:15 -0800

    0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
         print("{} arguments.".format(i))
     elif i == 1:
         print("{} argument:".format(i))
-        i = 1
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
+        i = 0
+        for arg1 in sys.argv:
+            if i == 1:
+                print("{}: {}".format(i, arg1))
             i += 1
         
     elif i >= 1:
