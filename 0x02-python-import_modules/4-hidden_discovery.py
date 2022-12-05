413422fcafad220293dc4b9538bda41018213008
elolengodfrey <elolengodfrey@gmail.com>
2022-12-05 00:30:42 -0800

    0

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.8.15
 if __name__ == "__main__":
     import hidden_4
-    for name in dir(hidden_4):
-        if name[] != '_':
-            print(name)
+    for s in dir(hidden_4):
+        if s[:2] != "__":
+            print("{:s}".format(s))
