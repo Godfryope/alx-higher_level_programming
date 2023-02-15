6069ae0ea40257c6394f35f9b85c13034c568cbe
elolengodfrey <elolengodfrey@gmail.com>
2023-02-15 03:13:05 +0100

    script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 -- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-mysql -u <username> -p -e "SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='$1' AND TABLE_NAME='first_table'" hbtn_0c_0
-
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='hbtn_0c_0' AND TABLE_NAME='first_table'
 
 
