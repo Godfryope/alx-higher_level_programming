f8dcc6e98894893414a8a5c83634b0fe5b585bfa
elolengodfrey <elolengodfrey@gmail.com>
2023-02-16 12:59:04 +0100

    script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-mysql -u -uroot -p hbtn_0d_tvshows < /path/to/dump/10-genre_id_by_show.sql
+mysql -u -uroot -p hbtn_0d_tvshows < ~/alx-higher_level_programming/0x0E-SQL_more_queries/10-genre_id_by_show.sql
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
