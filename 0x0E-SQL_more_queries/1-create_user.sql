43d5af29c6564c16d56a67f17638b823a325f4e9
elolengodfrey <elolengodfrey@gmail.com>
2023-02-16 12:01:24 +0100

    script that creates the database hbtn_0d_2 and the user user_0d_2.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 #!/bin/bash
 
--- script that creates the MySQL server user user_0d_1.
+-- script that creates the MySQL server user user_0d_1.x									
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
