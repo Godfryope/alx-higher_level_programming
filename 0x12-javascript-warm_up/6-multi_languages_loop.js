d5433ffc38165d15ebfba2df5e3f9477418aa3ea
elolengodfrey <elolengodfrey@gmail.com>
2023-03-14 04:28:23 +0100

    Write a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const languages = ['C', 'Python', 'JavaScript'];
+const adjectives = ['fun', 'cool', 'amazing'];
+
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i] + ' is ' + adjectives[i]);
+}
+
