f88e5fe70a9d23809cff3bff8981180f51ef2d56
elolengodfrey <elolengodfrey@gmail.com>
2024-06-21 12:24:35 +0100

    new

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,3 @@
-#!/bin/bash
-
--- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-mysql -u -uroot -p hbtn_0d_tvshows < ~/alx-higher_level_programming/0x0E-SQL_more_queries/10-genre_id_by_show.sql
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
-
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- lists all rows of a database that have one column in common
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
